8665731c996cdaeebb3159ed9118ab776840c98f
HetalBagal <hetalbagal@gmail.com>
2026-02-28 20:01:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Script.js initializing")
+let box= document.querySelector(".container").children
+
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1} , ${val2} , ${val3})`
+}
+
+Array.from(box).forEach(e=>{
+e.style.backgroundColor = getRandomColor()
+e.style.color = getRandomColor()
+})
